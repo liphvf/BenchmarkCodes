47e412317f9aa664eca79d84fc20ada2c33d403c
Filiphe Vilar Figueiredo <liphvf@outlook.com>
2023-08-25 01:08:21 -0300

    Organize output

--- BenchmarkCodes/BenchmarkTests/TasksTests.cs
+++ BenchmarkCodes/BenchmarkTests/TasksTests.cs
@@ -1,13 +1,16 @@
 using BenchmarkCodes.Data;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
 using Microsoft.EntityFrameworkCore;
 
 namespace BenchmarkCodes.BenchmarkTests;
 
 [MemoryDiagnoser]
 [SimpleJob(RuntimeMoniker.Net70)]
-// [SimpleJob(RuntimeMoniker.NativeAot70)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn(NumeralSystem.Arabic)]
 [RPlotExporter]
 public class TasksTests
 {
@@ -18,40 +21,37 @@ public class TasksTests
         _db = DatabaseContext.Create();
     }
 
-    [Benchmark(Description = "ValueSync")]
-    public ValueTask<Person> GetPersonWithSyncResponseUsingValueTaskAsync()
-    {
-        var person = new Person
-        {
-            Name = "John",
-            Surname = "Doe"
-        };
-
-        return new ValueTask<Person>(person);
-    }
-
+    [Arguments(true)]
+    [Arguments(false)]
     [Benchmark(Description = "ValueAsync")]
-    public async ValueTask<Person> GetPersonWithAsyncResponseUsingValueTaskAsync()
+    public async ValueTask<Person> GetPersonWithAsyncResponseUsingValueTaskAsync(bool sync)
     {
+        if (sync)
+        {
+            return new Person
+            {
+                Name = "John",
+                Surname = "Doe"
+            };
+        }
+
         var person = await _db.People.SingleAsync(p => p.Name == "John" && p.Surname == "Doe");
         return person;
     }
 
-    [Benchmark(Description = "TaskSync")]
-    public Task<Person> GetPersonWithSyncResponseUsingTaskAsync()
-    {
-        var person = new Person
-        {
-            Name = "John",
-            Surname = "Doe"
-        };
-
-        return Task.FromResult(person);
-    }
-
+    [Arguments(true)]
+    [Arguments(false)]
     [Benchmark(Description = "TaskAsync")]
-    public async Task<Person> GetPersonWithAsyncResponseUsingTaskAsync()
+    public async Task<Person> GetPersonWithAsyncResponseUsingTaskAsync(bool sync)
     {
+        if (sync)
+        {
+            return new Person
+            {
+                Name = "John",
+                Surname = "Doe"
+            };
+        }
         var person = await _db.People.SingleAsync(p => p.Name == "John" && p.Surname == "Doe");
         return person;
     }
